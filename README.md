9e989e0658d7664d6caf50eb47750abdeb12e1ec
Atma Bharathi Mani <atmabharathi@gmail.com>
2022-09-30 15:10:15 -0700

    adds ability to read from settings, adds animation to readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# sublime-format-argo
-Sublime Text plugin to format argo submit commands
+# Argo utils for Sublime Text
+This plugin can help you sort and pretty format your `argo submit ...` commands. To use it simply select the text and call for **Format argo** from the heads-up display as shown below:
+
+![gif showing format argo cmd](argo-utils-sublimetext2.gif)
+
+## Settings
+The tool can be configured. To edit the settings, go to **"Sublime Text > Preferences > Package Settings > Argo Utils"**
--- argo_utils.py
+++ argo_utils.py
@@ -1,3 +1,4 @@
+# store in ~/Library/Application\ Support/Sublime\ Text/Packages/User/format_argo.py
 import sublime
 import sublime_plugin
 
@@ -6,6 +7,9 @@ class FormatArgoCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		view = self.view
 		regions = view.sel()
+
+		# get settings
+		settings = sublime.load_settings("Argo Utils.sublime-settings")
 
 		# check if something is selected
 		if len(regions) > 1 or not regions[0].empty():
@@ -16,7 +20,7 @@ class FormatArgoCommand(sublime_plugin.TextCommand):
 					original_text = view.substr(region)
 
 					# modify
-					new_text = self._format_argo(original_text)
+					new_text = self._format_argo(original_text, settings)
 
 					# put back
 					view.replace(edit, region, new_text)
@@ -28,13 +32,17 @@ class FormatArgoCommand(sublime_plugin.TextCommand):
 			original_text = view.substr(all_text_region)
 
 			# modify
-			new_text = self._format_argo(original_text)
+			new_text = self._format_argo(original_text, settings)
 
 			# put back
 			view.replace(edit, region, new_text)
 
+		# set syntax to bash
+		if settings.get('set_syntax_to_markdown'):
+			view.assign_syntax('Packages/Markdown/Markdown.sublime-syntax')
+
 	
-	def _format_argo(self, argo_str):
+	def _format_argo(self, argo_str, settings):
 		"""
 		Formats input argo submit command
 		"""
@@ -42,16 +50,24 @@ class FormatArgoCommand(sublime_plugin.TextCommand):
 		prefix, *lines = argo_str.split(" -p ")
 
 		# sort lines
-		lines = sorted(lines)
+		if settings.get('alphabetize_parameters'):
+			lines = sorted(lines)
 
-		ret_str = ""
+		# encapsulate in triple ticks
+		if settings.get('encapsulate_in_triple_ticks'):
+			ret_str = "```bash\n"
+		else:
+			ret_str = ""
 
 		suffix="\\"
-		ret_str = f'{prefix} {suffix}\n'  # argo submit ... line
+		ret_str = ret_str + f'{prefix} {suffix}\n'  # argo submit ... line
 		for i, line in enumerate(lines):
 			a,b = line.split("=")
 			if i == len(lines)-1:  # last line
 				suffix= ""
 			ret_str = ret_str + f'-p {a}={b} {suffix}\n'
 			
+		if settings.get('encapsulate_in_triple_ticks'):
+			ret_str = ret_str + "\n```"
+
 		return ret_str
